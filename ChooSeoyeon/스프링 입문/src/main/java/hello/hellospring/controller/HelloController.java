71dae41db6a893e2cc578143668853e865e9c79c
ChooSeoYeon <choo000407@naver.com>
2022-09-26 18:22:16 +0900

    Docs(README.md): update README.md

--- ChooSeoyeon/스프링 입문/src/main/java/hello/hellospring/controller/HelloController.java
+++ ChooSeoyeon/스프링 입문/src/main/java/hello/hellospring/controller/HelloController.java
@@ -8,7 +8,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 @Controller
 public class HelloController {
     @GetMapping("hello")
-    public String hey(Model model) {
+    public String hello(Model model) {
         model.addAttribute("data", "hello!!");
         return "hello";
     }
