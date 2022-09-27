a98a5d55bb44a3c70c9634286bcb9b710cb339cd
ssssujini99 <sssssjin99@naver.com>
2022-09-28 02:09:26 +0900

    :sparkles: Feat: API - @ResponseBody 이용

--- ssssujini99/spring_introduction/src/main/java/hello/spring_introduction/controller/HelloController.java
+++ ssssujini99/spring_introduction/src/main/java/hello/spring_introduction/controller/HelloController.java
@@ -4,6 +4,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
 public class HelloController {
@@ -19,4 +20,31 @@ public class HelloController {
         model.addAttribute("name", name);
         return "hello-template";
     }
+
+    @GetMapping("hello-string")
+    @ResponseBody  // -> "HTTP의 BODY에" 문자 내용을 직접 반환
+    public String helloString(@RequestParam("name") String name) {
+        return "hello" + name;
+    }
+
+    @GetMapping("hello-api")
+    @ResponseBody
+    public Hello helloApi(@RequestParam("name") String name) {
+        Hello hello = new Hello();
+        hello.setName(name);
+        return hello; // @ResponseBody를 사용하고 객체를 반환하면 -> 객체가 JSON으로 변환됨
+    }
+
+    static class Hello {
+        private String name;
+
+        public String getName() {
+            return name;
+        }
+
+        public void setName(String name) {
+            this.name = name;
+        }
+    }
+
 }
