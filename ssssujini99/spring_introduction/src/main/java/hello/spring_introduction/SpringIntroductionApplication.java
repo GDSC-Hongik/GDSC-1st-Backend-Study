8901581f7c2c9b167ba5a78e596affebd299c109
ssssujini99 <sssssjin99@naver.com>
2022-09-27 21:07:28 +0900

    :tada: Settings(spring_introduction/): 스프링 입문 강의 프로젝트 세팅

---
+++ ssssujini99/spring_introduction/src/main/java/hello/spring_introduction/SpringIntroductionApplication.java
@@ -0,0 +1,13 @@
+package hello.spring_introduction;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringIntroductionApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringIntroductionApplication.class, args);
+	}
+
+}
