c54f0679613ebf07f78ec2b0a4df0f18e5368140
ssssujini99 <sssssjin99@naver.com>
2022-09-28 01:40:47 +0900

    :sparkles: Feat: MVC와 템플릿 엔진

--- ssssujini99/spring_introduction/src/main/java/hello/spring_introduction/controller/HelloController.java
+++ ssssujini99/spring_introduction/src/main/java/hello/spring_introduction/controller/HelloController.java
@@ -3,6 +3,7 @@ package hello.spring_introduction.controller;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 public class HelloController {
@@ -11,5 +12,11 @@ public class HelloController {
     public String hello(Model model) {
         model.addAttribute("data", "hello");
         return "hello";
+    }
+
+    @GetMapping("hello-mvc")
+    public String helloMvc(@RequestParam("name") String name, Model model) {
+        model.addAttribute("name", name);
+        return "hello-template";
     }
 }
