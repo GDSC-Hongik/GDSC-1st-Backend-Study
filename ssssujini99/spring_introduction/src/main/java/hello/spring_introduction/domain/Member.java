4469f467f75e88ba8373d7c1f985b3ab257ae310
ssssujini99 <sssssjin99@naver.com>
2022-09-28 02:38:00 +0900

    :sparkles: Feat: 회원 도메인 생성

---
+++ ssssujini99/spring_introduction/src/main/java/hello/spring_introduction/domain/Member.java
@@ -0,0 +1,23 @@
+package hello.spring_introduction.domain;
+
+public class Member {
+
+    private Long id;
+    private String name;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+}
