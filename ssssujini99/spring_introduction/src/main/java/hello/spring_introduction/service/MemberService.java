30459cf7fd05369821d3bc882f0e46ae2b3a7c5b
ssssujini99 <sssssjin99@naver.com>
2022-09-28 16:46:26 +0900

    :sparkles: Feat: 회원 서비스 테스트코드 작성(MemberServiceTest) & DI로 변경

--- ssssujini99/spring_introduction/src/main/java/hello/spring_introduction/service/MemberService.java
+++ ssssujini99/spring_introduction/src/main/java/hello/spring_introduction/service/MemberService.java
@@ -9,7 +9,12 @@ import java.util.Optional;
 
 public class MemberService {
 
-    private final MemberRepository memberRepository = new MemoryMemberRepository();
+    private final MemberRepository memberRepository;
+
+    // memberRepository를 외부에서 넣어줄 수 있게 바꿔주기 (new로 직접 생성하는 게 아니라)
+    public MemberService(MemberRepository memberRepository) {
+        this.memberRepository = memberRepository;
+    }
 
     // 회원 가입
     public Long join(Member member) {
