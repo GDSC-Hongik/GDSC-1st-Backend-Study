52fa848d0dd03da6a086f3b865bcdbbc2e8b2549
ssssujini99 <sssssjin99@naver.com>
2022-09-28 20:40:41 +0900

    :sparkles: Feat: 회원 웹 기능 - 조회

--- ssssujini99/spring_introduction/src/main/java/hello/spring_introduction/controller/MemberController.java
+++ ssssujini99/spring_introduction/src/main/java/hello/spring_introduction/controller/MemberController.java
@@ -4,8 +4,11 @@ import hello.spring_introduction.domain.Member;
 import hello.spring_introduction.service.MemberService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
+
+import java.util.List;
 
 @Controller
 public class MemberController {
@@ -29,6 +32,13 @@ public class MemberController {
 
         memberService.join(member);
         return "redirect:/";
+    }
+
+    @GetMapping("/members")
+    public String list(Model model) {
+        List<Member> members = memberService.findMembers();
+        model.addAttribute("members", members);
+        return "members/memberList";
     }
 
 }
