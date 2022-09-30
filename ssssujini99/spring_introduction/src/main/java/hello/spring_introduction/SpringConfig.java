2b98775c185e6d68509477e6fc74ab2bb23bed63
ssssujini99 <sssssjin99@naver.com>
2022-09-30 19:31:21 +0900

    :sparkles: Feat: 순수 JDBC / memberService에서 JdbcMemberRepository로 DI 해주기

--- ssssujini99/spring_introduction/src/main/java/hello/spring_introduction/SpringConfig.java
+++ ssssujini99/spring_introduction/src/main/java/hello/spring_introduction/SpringConfig.java
@@ -1,13 +1,24 @@
 package hello.spring_introduction;
 
+import hello.spring_introduction.repository.JdbcMemberRepository;
 import hello.spring_introduction.repository.MemberRepository;
 import hello.spring_introduction.repository.MemoryMemberRepository;
 import hello.spring_introduction.service.MemberService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
+import javax.sql.DataSource;
+
 @Configuration
 public class SpringConfig {
+
+    private DataSource dataSource;
+
+    @Autowired
+    public SpringConfig(DataSource dataSource) {
+        this.dataSource = dataSource;
+    }
 
     @Bean
     public MemberService memberService() {
@@ -16,6 +27,7 @@ public class SpringConfig {
 
     @Bean
     public MemberRepository memberRepository() {
-        return new MemoryMemberRepository();
+//        return new MemoryMemberRepository();
+        return new JdbcMemberRepository(dataSource);
     }
 }
