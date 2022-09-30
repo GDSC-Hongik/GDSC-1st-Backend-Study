ce8c95b93a74c779ba40f35489987801c4742fca
ssssujini99 <sssssjin99@naver.com>
2022-09-30 21:29:01 +0900

    :sparkles: Feat: 스프링 JdbcTemplate

--- ssssujini99/spring_introduction/src/main/java/hello/spring_introduction/SpringConfig.java
+++ ssssujini99/spring_introduction/src/main/java/hello/spring_introduction/SpringConfig.java
@@ -1,6 +1,7 @@
 package hello.spring_introduction;
 
 import hello.spring_introduction.repository.JdbcMemberRepository;
+import hello.spring_introduction.repository.JdbcTemplateMemberRepository;
 import hello.spring_introduction.repository.MemberRepository;
 import hello.spring_introduction.repository.MemoryMemberRepository;
 import hello.spring_introduction.service.MemberService;
@@ -28,6 +29,7 @@ public class SpringConfig {
     @Bean
     public MemberRepository memberRepository() {
 //        return new MemoryMemberRepository();
-        return new JdbcMemberRepository(dataSource);
+//        return new JdbcMemberRepository(dataSource);
+        return new JdbcTemplateMemberRepository(dataSource);
     }
 }
