0f8fce7dea57b3252aa4bf3050f7a9b80e8dbb00
ssssujini99 <sssssjin99@naver.com>
2022-10-04 19:33:38 +0900

    :sparkles: Feat: AOP 적용

--- ssssujini99/spring_introduction/src/main/java/hello/spring_introduction/service/MemberService.java
+++ ssssujini99/spring_introduction/src/main/java/hello/spring_introduction/service/MemberService.java
@@ -22,19 +22,9 @@ public class MemberService {
 
     // 회원 가입
     public Long join(Member member) {
-
-        long start = System.currentTimeMillis();
-
-        try{
-            validateDuplicateMember(member); // 중복 회원 검증
-            memberRepository.save(member);
-            return member.getId();
-
-        } finally {
-            long finish = System.currentTimeMillis();
-            long timeMs = finish - start;
-            System.out.println("join = " + timeMs + "ms");
-        }
+        validateDuplicateMember(member); // 중복 회원 검증
+        memberRepository.save(member);
+        return member.getId();
     }
 
     // 중복 회원 검증
@@ -47,14 +37,7 @@ public class MemberService {
 
     // 전체 회원 조회
     public List<Member> findMembers() {
-        long start = System.currentTimeMillis();
-        try{
-            return memberRepository.findAll();
-        } finally {
-            long finish = System.currentTimeMillis();
-            long timeMs = finish - start;
-            System.out.println("findMembers = " + timeMs + "ms");
-        }
+        return memberRepository.findAll();
     }
 
     // 회원 한 명 조회
