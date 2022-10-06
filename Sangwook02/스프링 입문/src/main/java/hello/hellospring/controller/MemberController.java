c1bf5f6e1d1eddfc3d7b7483b8bddc76da92c16c
Cho Sangwook <82208159+Sangwook02@users.noreply.github.com>
2022-10-06 15:12:56 +0900

    Feat: 의존 관계 설정

---
+++ Sangwook02/스프링 입문/src/main/java/hello/hellospring/controller/MemberController.java
@@ -0,0 +1,14 @@
+package hello.hellospring.controller;
+import hello.hellospring.service.MemberService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+
+@Controller
+public class MemberController {
+    private final MemberService memberService;
+
+    @Autowired
+    public MemberController(MemberService memberService) {
+        this.memberService = memberService;
+    }
+}
