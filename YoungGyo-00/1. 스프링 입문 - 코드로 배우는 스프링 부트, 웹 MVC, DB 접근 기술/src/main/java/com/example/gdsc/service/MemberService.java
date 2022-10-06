6cf333f4c888c5f724a05b685910fcc8d5de4ab6
younggyo <lee4519698@hanmail.net>
2022-10-06 10:14:32 +0900

    Feat: AOP 적용 후 코드

--- YoungGyo-00/1. 스프링 입문 - 코드로 배우는 스프링 부트, 웹 MVC, DB 접근 기술/src/main/java/com/example/gdsc/service/MemberService.java
+++ YoungGyo-00/1. 스프링 입문 - 코드로 배우는 스프링 부트, 웹 MVC, DB 접근 기술/src/main/java/com/example/gdsc/service/MemberService.java
@@ -17,7 +17,8 @@ public class MemberService {
     }
 
     public Long join(Member member) {
-        validateDuplicateMember(member); //중복 회원 검증 memberRepository.save(member);
+        validateDuplicateMember(member); //중복 회원 검증
+        memberRepository.save(member);
         return member.getId();
     }
     private void validateDuplicateMember(Member member) {
