2e3fc67bd268877cfb2d894234324c5e418b0de3
danhandev <danhandev@gmail.com>
2022-10-31 01:59:32 +0900

    ✨FEAT(핵심원리): 주문과 할인 도메인 실행과 테스트

---
+++ danhandev/스프링 핵심원리 기본편/core/src/main/java/hello/core/order/OrderApp.java
@@ -0,0 +1,19 @@
+package hello.core.order;
+
+import hello.core.member.Grade;
+import hello.core.member.Member;
+import hello.core.member.MemberService;
+import hello.core.member.MemberServiceImpl;
+
+public class OrderApp {
+    public static void main(String[] args) {
+        MemberService memberService = new MemberServiceImpl();
+        OrderService orderService = new OrderServiceImpl();
+
+        Long memberId = 1L;
+        Member member = new Member(memberId, "memberA", Grade.VIP);
+        memberService.join(member);
+        Order order = orderService.createOrder(memberId, "itemA", 10000);
+        System.out.println("order = " + order);
+    }
+}
---
+++ danhandev/스프링 핵심원리 기본편/core/src/test/java/hello/core/member/MemberServiceTest.java
@@ -0,0 +1,23 @@
+package hello.core.member;
+
+import hello.core.member.Grade;
+import hello.core.member.Member;
+import hello.core.member.MemberService;
+import hello.core.member.MemberServiceImpl;
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+public class MemberServiceTest {
+
+    MemberService memberService = new MemberServiceImpl();
+    @Test
+    void join() {
+        //given
+        Member member = new Member(1L, "memberA", Grade.VIP);
+        //when
+        memberService.join(member);
+        Member findMember = memberService.findMember(1L);
+        //then
+        Assertions.assertThat(member).isEqualTo(findMember);
+    }
+}
