3e4076fefc54269cc29ffe1ff1856513473e22e2
ssssujini99 <sssssjin99@naver.com>
2022-10-31 16:11:02 +0900

    :goal_net: Error(DIP 위반): OrderServiceImpl은 인터페이스 + 구체클레스에도 함께 의존함

--- ssssujini99/spring_core/src/main/java/hello/spring_core/order/OrderServiceImpl.java
+++ ssssujini99/spring_core/src/main/java/hello/spring_core/order/OrderServiceImpl.java
@@ -2,6 +2,7 @@ package hello.spring_core.order;
 
 import hello.spring_core.discount.DiscountPolicy;
 import hello.spring_core.discount.FixDiscountPolicy;
+import hello.spring_core.discount.RateDiscountPolicy;
 import hello.spring_core.member.Member;
 import hello.spring_core.member.MemberRepository;
 import hello.spring_core.member.MemoryMemberRepository;
@@ -9,7 +10,7 @@ import hello.spring_core.member.MemoryMemberRepository;
 public class OrderServiceImpl implements OrderService {
 
     private final MemberRepository memberRepository = new MemoryMemberRepository(); // 메모리 멤버 리포지토리
-    private final DiscountPolicy discountPolicy = new FixDiscountPolicy(); // 고정 할인 정책
+    private DiscountPolicy discountPolicy; // 인터페이스에만 의존하도록 코드 변경
 
     @Override
     public Order createOrder(Long memberId, String itemName, int itemPrice) {
