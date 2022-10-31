3170bd3433cf2c8fb1420c20a2af023d20de7e75
ssssujini99 <sssssjin99@naver.com>
2022-10-31 16:08:51 +0900

    :sparkles: Feat: 새로운 할인 정책 개발 - RateDiscountPolicy

---
+++ ssssujini99/spring_core/src/main/java/hello/spring_core/discount/RateDiscountPolicy.java
@@ -0,0 +1,18 @@
+package hello.spring_core.discount;
+
+import hello.spring_core.member.Grade;
+import hello.spring_core.member.Member;
+
+public class RateDiscountPolicy implements DiscountPolicy {
+
+    private int discountPercent = 10;
+
+    @Override
+    public int discount(Member member, int price) {
+        if (member.getGrade() == Grade.VIP) {
+            return price * discountPercent / 100;
+        } else {
+            return 0;
+        }
+    }
+}
