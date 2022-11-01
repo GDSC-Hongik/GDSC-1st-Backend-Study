14ef4194674b57f54cf1016f5ff64d15420b38be
younggyo <lee4519698@hanmail.net>
2022-11-02 06:45:25 +0900

    Test : 주문 할인 서비스 테스트 코드

--- YoungGyo-00/2.스프링 핵심 기술 - 입문편/src/main/java/com/example/hello/MemberApp.java
+++ YoungGyo-00/2.스프링 핵심 기술 - 입문편/src/main/java/com/example/hello/MemberApp.java
@@ -1,4 +1,9 @@
-package com.example.hello.member;
+package com.example.hello;
+
+import com.example.hello.member.Grade;
+import com.example.hello.member.Member;
+import com.example.hello.member.MemberService;
+import com.example.hello.member.MemberServiceImpl;
 
 public class MemberApp {
     public static void main(String[] args) {
---
+++ YoungGyo-00/2.스프링 핵심 기술 - 입문편/src/test/java/com/example/hello/order/OrderServiceTest.java
@@ -0,0 +1,24 @@
+package com.example.hello.order;
+
+import com.example.hello.member.Grade;
+import com.example.hello.member.Member;
+import com.example.hello.member.MemberService;
+import com.example.hello.member.MemberServiceImpl;
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+public class OrderServiceTest {
+
+    MemberService memberService = new MemberServiceImpl();
+    OrderService orderService = new OrderServiceImpl();
+
+    @Test
+    void createOrder() {
+        Long memberId = 1L;
+        Member member = new Member(memberId, "memberA", Grade.VIP);
+        memberService.join(member);
+
+        Order order = orderService.createOrder(memberId, "itemA", 10000);
+        Assertions.assertThat(order.getDiscountPrice()).isEqualTo(1000);
+    }
+}
