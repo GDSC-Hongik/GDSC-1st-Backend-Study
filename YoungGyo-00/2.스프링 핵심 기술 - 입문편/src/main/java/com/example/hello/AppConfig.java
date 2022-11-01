092899d8046746ad1f9f2438881dda548bd4262c
younggyo <lee4519698@hanmail.net>
2022-11-02 08:02:17 +0900

    Refactor : 순수 자바 -> 스프링 적용 (AppConfig)

--- YoungGyo-00/2.스프링 핵심 기술 - 입문편/src/main/java/com/example/hello/AppConfig.java
+++ YoungGyo-00/2.스프링 핵심 기술 - 입문편/src/main/java/com/example/hello/AppConfig.java
@@ -2,22 +2,30 @@ package com.example.hello;
 
 import com.example.hello.discount.DiscountPolicy;
 import com.example.hello.discount.FixDiscountPolicy;
+import com.example.hello.discount.RateDiscountPolicy;
 import com.example.hello.member.MemberRepository;
 import com.example.hello.member.MemberService;
 import com.example.hello.member.MemberServiceImpl;
 import com.example.hello.member.MemoryMemberRepository;
 import com.example.hello.order.OrderService;
 import com.example.hello.order.OrderServiceImpl;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
 
+@Configuration
 public class AppConfig {
+
+    @Bean
     public MemberService memberService() {
         return new MemberServiceImpl(memberRepository());
     }
 
+    @Bean
     public MemberRepository memberRepository() {
         return new MemoryMemberRepository();
     }
 
+    @Bean
     public OrderService orderService() {
         return new OrderServiceImpl(
                 memberRepository(),
@@ -25,7 +33,9 @@ public class AppConfig {
         );
     }
 
+    @Bean
     public DiscountPolicy discountPolicy() {
-        return new FixDiscountPolicy();
+        // return new FixDiscountPolicy();
+        return new RateDiscountPolicy();
     }
 }
