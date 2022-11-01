06f778bbe7f521c963d077e8a841a2f2cbd96700
younggyo <lee4519698@hanmail.net>
2022-11-02 06:21:09 +0900

    Feat : 회원 도메인, 레포지토리, 서비스 개발 및 실행문

---
+++ YoungGyo-00/2.스프링 핵심 기술 - 입문편/src/main/java/com/example/hello/member/MemberApp.java
@@ -0,0 +1,13 @@
+package com.example.hello.member;
+
+public class MemberApp {
+    public static void main(String[] args) {
+        MemberService memberService = new MemberServiceImpl();
+        Member member = new Member(1L, "memberA", Grade.VIP);
+        memberService.join(member);
+
+        Member findMember = memberService.findMember(1L);
+        System.out.println("new member = " + member.getName());
+        System.out.println("find Member = " + findMember.getName());
+    }
+}
