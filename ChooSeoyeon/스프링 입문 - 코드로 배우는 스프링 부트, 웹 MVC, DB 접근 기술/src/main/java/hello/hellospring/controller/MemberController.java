b1c36ff6d67b5ca5a27554c9ccfdb0ab2b197960
ChooSeoYeon <choo000407@naver.com>
2022-11-02 06:08:24 +0900

    Feat: 회원 웹 기능-홈 화면 추가, 등록

--- ChooSeoyeon/스프링 입문 - 코드로 배우는 스프링 부트, 웹 MVC, DB 접근 기술/src/main/java/hello/hellospring/controller/MemberController.java
+++ ChooSeoyeon/스프링 입문 - 코드로 배우는 스프링 부트, 웹 MVC, DB 접근 기술/src/main/java/hello/hellospring/controller/MemberController.java
@@ -1,19 +1,39 @@
 // 4.스프링 빈과 의존관계 - 1) 컴포넌트 스캔과 자동 의존관계 설정
+// 5. 회원 관리 예제_웹 MVC 개발 - 2) 회원 웹 기능-등록
 
 package hello.hellospring.controller;
 
-import hello.hellospring.service.MemberService; // import(MemberService)
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Controller; // import(@Contorller)
+import hello.hellospring.domain.Member; // import Member 5.2
+import hello.hellospring.service.MemberService; // import(MemberService) 4.1
+import org.springframework.beans.factory.annotation.Autowired; // import Autowired 4.1
+import org.springframework.stereotype.Controller; // import(@Contorller) 4.1
+import org.springframework.web.bind.annotation.GetMapping; // import GetMapping 5.1
+import org.springframework.web.bind.annotation.PostMapping; // import PostMapping 5.2
 
-@Controller // import(@Contorller)
+@Controller // import(@Contorller) 4.1
 public class MemberController {
-    // private final MemberService memberService = new MemberService(); // import(MemberService). 수정 전(MemberService를 new로 생성)
+    // private final MemberService memberService = new MemberService(); // import(MemberService) 4.1. 수정 전(MemberService를 new로 생성)
     private final MemberService memberService; // 스프링 컨테이너에 등록
 
     // 생성자(Alt+Insert)로 연결해줌
-    @Autowired
+    @Autowired // import Autowired 4.1
     public MemberController(MemberService memberService) {
         this.memberService = memberService;
+    }
+
+    // 5.2
+    @GetMapping("/members/new") // import GetMapping 5.1
+    public String createForm() {
+        return "members/createMemberForm";
+    }
+
+    // 5.2
+    @PostMapping("/members/new") // import PostMapping 5.2
+    public String create(MemberForm form) { //  form으로 name 들어옴 (스프링이 setname으로 값 넣어놨음)
+        Member member = new Member(); // import Member 5.2 - 주의) 우리가 만든 hello.hellospring.domain의 Member import하기
+        member.setName(form.getName());
+        // System.out.println("member = " + member.getName());
+        memberService.join(member);
+        return "redirect:/"; // 회원가입 끝났으니 홈 화면으로 보냄
     }
 }
---
+++ ChooSeoyeon/스프링 입문 - 코드로 배우는 스프링 부트, 웹 MVC, DB 접근 기술/src/main/java/hello/hellospring/controller/MemberForm.java
@@ -0,0 +1,15 @@
+// 5. 회원 관리 예제_웹 MVC 개발 - 2) 회원 웹 기능-등록
+
+package hello.hellospring.controller;
+
+public class MemberForm {
+    private String name;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+}
