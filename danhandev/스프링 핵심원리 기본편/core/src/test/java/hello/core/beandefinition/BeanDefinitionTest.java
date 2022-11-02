d4d28274e141768f2be5553e4fc5a7cd7baadb80
danhandev <danhandev@gmail.com>
2022-11-02 12:44:12 +0900

    ✨FEAT(핵심원리): 스프링 빈 설정 메타 정보 - BeanDefinition

---
+++ danhandev/스프링 핵심원리 기본편/core/src/test/java/hello/core/beandefinition/BeanDefinitionTest.java
@@ -0,0 +1,28 @@
+package hello.core.beandefinition;
+import hello.core.AppConfig;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.MutablePropertyValues;
+import org.springframework.beans.factory.config.BeanDefinition;
+import org.springframework.beans.factory.config.ConstructorArgumentValues;
+import
+        org.springframework.context.annotation.AnnotationConfigApplicationContext;
+import org.springframework.context.support.GenericXmlApplicationContext;
+public class BeanDefinitionTest {
+    AnnotationConfigApplicationContext ac = new
+            AnnotationConfigApplicationContext(AppConfig.class);
+    // GenericXmlApplicationContext ac = new GenericXmlApplicationContext("appConfig.xml");
+    @Test
+    @DisplayName("빈 설정 메타정보 확인")
+    void findApplicationBean() {
+        String[] beanDefinitionNames = ac.getBeanDefinitionNames();
+        for (String beanDefinitionName : beanDefinitionNames) {
+            BeanDefinition beanDefinition =
+                    ac.getBeanDefinition(beanDefinitionName);
+            if (beanDefinition.getRole() == BeanDefinition.ROLE_APPLICATION) {
+                System.out.println("beanDefinitionName" + beanDefinitionName +
+                        " beanDefinition = " + beanDefinition);
+            }
+        }
+    }
+}
