cacf5566517d10f62b376e934767b607ab01afc4
danhandev <danhandev@gmail.com>
2022-11-02 14:40:00 +0900

    ✨FEAT(핵심원리): 싱글톤 컨테이너

--- danhandev/스프링 핵심원리 기본편/core/src/test/java/hello/core/singleton/SingletonTest.java
+++ danhandev/스프링 핵심원리 기본편/core/src/test/java/hello/core/singleton/SingletonTest.java
@@ -3,6 +3,9 @@ import hello.core.AppConfig;
 import hello.core.member.MemberService;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+
 import static org.assertj.core.api.Assertions.*;
 public class SingletonTest {
     @Test
@@ -38,5 +41,24 @@ public class SingletonTest {
         // singletonService1 == singletonService2
         assertThat(singletonService1).isSameAs(singletonService2);
         singletonService1.logic();
+    }
+
+    @Test
+    @DisplayName("스프링 컨테이너와 싱글톤")
+    void springContainer() {
+        ApplicationContext ac = new AnnotationConfigApplicationContext(AppConfig.class);
+
+        //1. 조회: 호출할 때 마다 같은 객체를 반환
+        MemberService memberService1 = ac.getBean("memberService", MemberService.class);
+
+        //2. 조회: 호출할 때 마다 같은 객체를 반환
+        MemberService memberService2 = ac.getBean("memberService", MemberService.class);
+
+        //참조값이 같은 것을 확인
+        System.out.println("memberService1 = " + memberService1);
+        System.out.println("memberService2 = " + memberService2);
+
+        //memberService1 == memberService2
+        assertThat(memberService1).isSameAs(memberService2);
     }
 }
