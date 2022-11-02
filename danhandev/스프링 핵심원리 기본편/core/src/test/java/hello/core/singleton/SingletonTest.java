cc04ab1ad8836535c095d3e67d377cdfb32662eb
danhandev <danhandev@gmail.com>
2022-11-02 14:33:32 +0900

    ✨FEAT(핵심원리): 싱글톤 패턴

--- danhandev/스프링 핵심원리 기본편/core/src/test/java/hello/core/singleton/SingletonTest.java
+++ danhandev/스프링 핵심원리 기본편/core/src/test/java/hello/core/singleton/SingletonTest.java
@@ -22,4 +22,21 @@ public class SingletonTest {
         //memberService1 != memberService2
         assertThat(memberService1).isNotSameAs(memberService2);
     }
+
+    @Test
+    @DisplayName("싱글톤 패턴을 적용한 객체 사용")
+    public void singletonServiceTest() {
+        //private으로 생성자를 막아두었다. 컴파일 오류가 발생한다.
+        //new SingletonService();
+        //1. 조회: 호출할 때 마다 같은 객체를 반환
+        SingletonService singletonService1 = SingletonService.getInstance();
+        //2. 조회: 호출할 때 마다 같은 객체를 반환
+        SingletonService singletonService2 = SingletonService.getInstance();
+        //참조값이 같은 것을 확인
+        System.out.println("singletonService1 = " + singletonService1);
+        System.out.println("singletonService2 = " + singletonService2);
+        // singletonService1 == singletonService2
+        assertThat(singletonService1).isSameAs(singletonService2);
+        singletonService1.logic();
+    }
 }
