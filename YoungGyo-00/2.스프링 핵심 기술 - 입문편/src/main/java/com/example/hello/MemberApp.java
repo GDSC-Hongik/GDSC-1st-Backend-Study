8650e47c302d7d43eaa4b1127e662dc7bd769e9a
younggyo <lee4519698@hanmail.net>
2022-11-02 18:42:06 +0900

    Test : 스프링 빈 테스트

--- YoungGyo-00/2.스프링 핵심 기술 - 입문편/src/main/java/com/example/hello/MemberApp.java
+++ YoungGyo-00/2.스프링 핵심 기술 - 입문편/src/main/java/com/example/hello/MemberApp.java
@@ -12,6 +12,7 @@ public class MemberApp {
 //        AppConfig appConfig = new AppConfig();
 //        MemberService memberService = appConfig.memberService();
 
+        // 스프링 컨테이너 생성
         ApplicationContext applicationContext = new AnnotationConfigApplicationContext(AppConfig.class);
         MemberService memberService = applicationContext.getBean("memberService", MemberService.class);
         Member member = new Member(1L, "memberA", Grade.VIP);
