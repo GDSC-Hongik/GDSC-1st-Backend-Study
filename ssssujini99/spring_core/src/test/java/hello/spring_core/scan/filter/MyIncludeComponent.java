7f458e646d54e859c00d63e6dbfc260fe9611f27
ssssujini99 <sssssjin99@naver.com>
2022-11-07 02:17:09 +0900

    :sparkles: Feat: 필터

---
+++ ssssujini99/spring_core/src/test/java/hello/spring_core/scan/filter/MyIncludeComponent.java
@@ -0,0 +1,11 @@
+package hello.spring_core.scan.filter;
+
+
+import java.lang.annotation.*;
+
+@Target(ElementType.TYPE)
+@Retention(RetentionPolicy.RUNTIME)
+@Documented
+public @interface MyIncludeComponent {
+
+}
