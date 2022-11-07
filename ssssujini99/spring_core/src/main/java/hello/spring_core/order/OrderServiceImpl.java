947d3447cfdb68d1c09e62538eb5181b316011b8
ssssujini99 <sssssjin99@naver.com>
2022-11-07 16:11:53 +0900

    :sparkles: Feat: Lombok 적용

--- ssssujini99/spring_core/src/main/java/hello/spring_core/order/OrderServiceImpl.java
+++ ssssujini99/spring_core/src/main/java/hello/spring_core/order/OrderServiceImpl.java
@@ -6,20 +6,22 @@ import hello.spring_core.discount.RateDiscountPolicy;
 import hello.spring_core.member.Member;
 import hello.spring_core.member.MemberRepository;
 import hello.spring_core.member.MemoryMemberRepository;
+import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
 @Component
+@RequiredArgsConstructor
 public class OrderServiceImpl implements OrderService {
 
     private final MemberRepository memberRepository;
     private final DiscountPolicy discountPolicy;
 
-    @Autowired
-    public OrderServiceImpl(MemberRepository memberRepository, DiscountPolicy discountPolicy) {
-        this.memberRepository = memberRepository;
-        this.discountPolicy = discountPolicy;
-    }
+//    @Autowired
+//    public OrderServiceImpl(MemberRepository memberRepository, DiscountPolicy discountPolicy) {
+//        this.memberRepository = memberRepository;
+//        this.discountPolicy = discountPolicy;
+//    }
 
 
     @Override
