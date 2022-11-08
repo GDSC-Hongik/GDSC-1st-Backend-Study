fd49cb1f91118753c3bad478517c39a6db1d725a
younggyo <lee4519698@hanmail.net>
2022-11-09 03:30:48 +0900

    Feat : 빈 스코프

---
+++ YoungGyo-00/2.스프링 핵심 기술 - 입문편/src/test/java/com/example/hello/scope/SingletonTest.java
@@ -0,0 +1,38 @@
+package com.example.hello.scope;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+import org.springframework.context.annotation.Scope;
+
+import javax.annotation.PostConstruct;
+import javax.annotation.PreDestroy;
+
+import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
+
+public class SingletonTest {
+
+    @Test
+    public void singletonBeanFind() {
+        AnnotationConfigApplicationContext ac = new AnnotationConfigApplicationContext(SingletonBean.class);
+        SingletonBean singletonBean1 = ac.getBean(SingletonBean.class);
+        SingletonBean singletonBean2 = ac.getBean(SingletonBean.class);
+        System.out.println("singletonBean1 = " + singletonBean1);
+        System.out.println("singletonBean2 = " + singletonBean2);
+        assertThat(singletonBean1).isSameAs(singletonBean2);
+        ac.close();
+    }
+
+    @Scope("singleton")
+    static class SingletonBean {
+
+        @PostConstruct
+        public void init() {
+            System.out.println("SingletonBean.init");
+        }
+
+        @PreDestroy
+        public void destory() {
+            System.out.println("SingletonBean.destory");
+        }
+    }
+}
