060bee3d3f3c0ae85b316a146051b6a8f2c60455
younggyo <lee4519698@hanmail.net>
2022-11-10 07:23:34 +0900

    Feat : JPA 동작 설정 확인

---
+++ YoungGyo-00/3. 실전! 스프링 부트와 JPA 활용 1편/src/test/java/jpabook/japshop/MemberRepositoryTest.java
@@ -0,0 +1,33 @@
+package jpabook.japshop;
+
+import org.assertj.core.api.Assertions;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.annotation.Rollback;
+import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.transaction.annotation.Transactional;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class MemberRepositoryTest {
+
+    @Autowired MemberRepository memberRepository;
+
+    @Test
+    @Transactional
+    @Rollback(value = false)
+    public void testMember () throws Exception {
+        // given
+        Member member = new Member();
+        member.setUsername("user");
+        // when
+        Long saveId = memberRepository.save(member);
+        Member findMember = memberRepository.find(saveId);
+        // then
+        Assertions.assertThat(findMember.getId()).isEqualTo(member.getId());
+        Assertions.assertThat(findMember.getUsername()).isEqualTo(member.getUsername());
+    }
+        
+}
