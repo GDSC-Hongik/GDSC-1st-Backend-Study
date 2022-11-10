d47cd88d6dc0f8e80f2c91dfa024679677dd5c1d
younggyo <lee4519698@hanmail.net>
2022-11-10 10:11:09 +0900

    Fix : 상품 도메인 기능

--- YoungGyo-00/3. 실전! 스프링 부트와 JPA 활용 1편/src/main/java/jpabook/japshop/domain/item/Item.java
+++ YoungGyo-00/3. 실전! 스프링 부트와 JPA 활용 1편/src/main/java/jpabook/japshop/domain/item/Item.java
@@ -1,6 +1,7 @@
 package jpabook.japshop.domain.item;
 
 import jpabook.japshop.domain.Category;
+import jpabook.japshop.exception.NotEnoughStockException;
 import lombok.Getter;
 import lombok.Setter;
 
@@ -26,4 +27,16 @@ public class Item {
 
     @ManyToMany(mappedBy = "items")
     private List<Category> categories = new ArrayList<Category>();
+
+    public void addStock(int quantity) {
+        this.stockQuantity += quantity;
+    }
+
+    public void removeStock(int quantity) {
+        int restStock = this.stockQuantity - quantity;
+        if (restStock < 0) {
+            throw new NotEnoughStockException("need more stock");
+        }
+        this.stockQuantity = restStock;
+    }
 }
