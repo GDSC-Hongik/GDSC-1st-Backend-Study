74959c1a70f879ee2f83929f3679e6a556dccf10
ssssujini99 <sssssjin99@naver.com>
2022-11-10 13:49:41 +0900

    :sparkles: Feat: 회원 서비스 개발

--- ssssujini99/jpashop/src/main/java/jpabook/jpashop/domain/Order.java
+++ ssssujini99/jpashop/src/main/java/jpabook/jpashop/domain/Order.java
@@ -18,7 +18,8 @@ public class Order {
     @Column(name = "order_id")
     private Long id;
 
-    @ManyToOne(fetch = FetchType.LAZY)
+    // @ManyToOne(fetch = FetchType.LAZY)
+    @ManyToOne
     @JoinColumn(name = "member_id")
     private Member member;
 
--- ssssujini99/jpashop/src/main/java/jpabook/jpashop/repository/MemberRepository.java
+++ ssssujini99/jpashop/src/main/java/jpabook/jpashop/repository/MemberRepository.java
@@ -1,6 +1,7 @@
 package jpabook.jpashop.repository;
 
 import jpabook.jpashop.domain.Member;
+import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Repository;
 
 import javax.persistence.EntityManager;
@@ -8,10 +9,10 @@ import javax.persistence.PersistenceContext;
 import java.util.List;
 
 @Repository
+@RequiredArgsConstructor
 public class MemberRepository {
 
-    @PersistenceContext
-    private EntityManager em;
+    private final EntityManager em;
 
     public void save(Member member) {
         em.persist(member);
@@ -26,7 +27,7 @@ public class MemberRepository {
     }
 
     public List<Member> findByName(String name) {
-        return em.createQuery("select m from Memer m where m.name = :name", Member.class)
+        return em.createQuery("select m from Member m where m.name = :name", Member.class)
                 .setParameter("name", name)
                 .getResultList();
     }
