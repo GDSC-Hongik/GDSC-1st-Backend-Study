10f9efabb8ed44e3a1e187bf8c65ac6cef6e21c8
uwoobeat <desktop3711@g.hongik.ac.kr>
2022-11-12 03:41:18 +0900

    Test: Add JUnit test and pure java test

---
+++ uwoobeat/스프링 핵심 기본편/src/main/java/hello/core/MemberApp.java
@@ -0,0 +1,19 @@
+package hello.core;
+
+import hello.core.member.Grade;
+import hello.core.member.Member;
+import hello.core.member.MemberService;
+import hello.core.member.MemberServiceImpl;
+
+public class MemberApp {
+
+    public static void main(String[] args) {
+        MemberService memberService = new MemberServiceImpl();
+        Member member = new Member(1L, "memberA", Grade.VIP);
+        memberService.join(member);
+
+        Member findMember = memberService.findMember(1L);
+        System.out.println("new member = " + member.getName());
+        System.out.println("find Member = " + findMember.getName());
+    }
+}
