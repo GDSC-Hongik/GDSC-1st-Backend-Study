21a7f549788686ebf53e7b1ee18a7e402e1e524d
ChooSeoYeon <choo000407@naver.com>
2022-11-14 03:35:52 +0900

    Feat: lombok 적용

--- ChooSeoyeon/스프링 부트와 JPA 활용1/jpashop/src/main/java/jpabook/jpashop/JpashopApplication.java
+++ ChooSeoyeon/스프링 부트와 JPA 활용1/jpashop/src/main/java/jpabook/jpashop/JpashopApplication.java
@@ -7,6 +7,12 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class JpashopApplication {
 
 	public static void main(String[] args) {
+
+		Hello hello = new Hello();
+		hello.setData("hello");
+		String data = hello.getData();
+		System.out.println("data = "+ data);
+
 		SpringApplication.run(JpashopApplication.class, args);
 	}
 
