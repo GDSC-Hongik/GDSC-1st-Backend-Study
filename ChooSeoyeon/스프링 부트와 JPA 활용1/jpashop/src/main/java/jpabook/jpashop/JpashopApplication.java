6d8da7d9dd4b9a94cc998943fa344cbc7b2952f2
ChooSeoYeon <choo000407@naver.com>
2022-11-14 04:50:14 +0900

    Feat: View 환경 설정

--- ChooSeoyeon/스프링 부트와 JPA 활용1/jpashop/src/main/java/jpabook/jpashop/JpashopApplication.java
+++ ChooSeoyeon/스프링 부트와 JPA 활용1/jpashop/src/main/java/jpabook/jpashop/JpashopApplication.java
@@ -1,4 +1,4 @@
-//1. 프로젝트 환경설정 - 1) 프로젝트 생성
+// 1. 프로젝트 환경설정 - 1) 프로젝트 생성
 
 package jpabook.jpashop;
 
@@ -10,10 +10,10 @@ public class JpashopApplication {
 
 	public static void main(String[] args) {
 		// 1.1 lombok 확인
-		Hello hello = new Hello();
-		hello.setData("hello");
-		String data = hello.getData();
-		System.out.println("data = "+ data);
+//		Hello hello = new Hello();
+//		hello.setData("hello");
+//		String data = hello.getData();
+//		System.out.println("data = "+ data);
 
 		SpringApplication.run(JpashopApplication.class, args);
 	}
