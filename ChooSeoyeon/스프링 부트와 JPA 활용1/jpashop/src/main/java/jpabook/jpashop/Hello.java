8bb2c45dbe6053631de97b045753d08f5e083fe5
ChooSeoYeon <choo000407@naver.com>
2022-11-14 03:52:59 +0900

    Style: 주석 추가

--- ChooSeoyeon/스프링 부트와 JPA 활용1/jpashop/src/main/java/jpabook/jpashop/Hello.java
+++ ChooSeoyeon/스프링 부트와 JPA 활용1/jpashop/src/main/java/jpabook/jpashop/Hello.java
@@ -1,3 +1,5 @@
+//1. 프로젝트 환경설정 - 1) 프로젝트 생성 -> lombok 확인
+
 package jpabook.jpashop;
 
 import lombok.Getter;
--- ChooSeoyeon/스프링 부트와 JPA 활용1/jpashop/src/main/java/jpabook/jpashop/JpashopApplication.java
+++ ChooSeoyeon/스프링 부트와 JPA 활용1/jpashop/src/main/java/jpabook/jpashop/JpashopApplication.java
@@ -1,3 +1,5 @@
+//1. 프로젝트 환경설정 - 1) 프로젝트 생성
+
 package jpabook.jpashop;
 
 import org.springframework.boot.SpringApplication;
@@ -7,7 +9,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class JpashopApplication {
 
 	public static void main(String[] args) {
-
+		// 1.1 lombok 확인
 		Hello hello = new Hello();
 		hello.setData("hello");
 		String data = hello.getData();
