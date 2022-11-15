68916795bce123d735fd3bef4429488be5c1cf47
Jaehyun Ahn <desktop3711@g.hongik.ac.kr>
2022-11-15 20:08:58 +0900

    Refactor: Refactor AppConfig to split interface and implementation

--- uwoobeat/스프링 핵심 기본편/src/main/java/hello/core/order/AppConfig.java
+++ uwoobeat/스프링 핵심 기본편/src/main/java/hello/core/order/AppConfig.java
@@ -1,20 +1,24 @@
 package hello.core.order;
 
+import hello.core.discount.DiscountPolicy;
 import hello.core.discount.FixDiscountPolicy;
+import hello.core.member.MemberRepository;
 import hello.core.member.MemberService;
 import hello.core.member.MemberServiceImpl;
 import hello.core.member.MemoryMemberRepository;
 
 public class AppConfig {
-
     public MemberService memberService() {
-        return new MemberServiceImpl(new MemoryMemberRepository());
+        return new MemberServiceImpl(memberRepository());
     }
-
     public OrderService orderService() {
         return new OrderServiceImpl(
-                new MemoryMemberRepository(),
-                new FixDiscountPolicy()
-        );
+                memberRepository(),
+                discountPolicy());
     }
-}
+    public MemberRepository memberRepository() {
+        return new MemoryMemberRepository();
+    }
+    public DiscountPolicy discountPolicy() {
+        return new FixDiscountPolicy();
+    }
