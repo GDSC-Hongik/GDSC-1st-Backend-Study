cbfdbcf56135158ee5214e2e1631e9ece60f24c7
ssssujini99 <sssssjin99@naver.com>
2022-11-16 21:53:06 +0900

    :sparkles: Feat: 주문, 주문상품 엔티티 개발

--- ssssujini99/jpashop/src/main/java/jpabook/jpashop/domain/Order.java
+++ ssssujini99/jpashop/src/main/java/jpabook/jpashop/domain/Order.java
@@ -50,4 +50,46 @@ public class Order {
         this.delivery = delivery;
         delivery.setOrder(this);
     }
+
+
+    // == 생성 메서드 == //
+    public static Order createOrder(Member member, Delivery delivery, OrderItem... orderItems) {
+        Order order = new Order();
+        order.setMember(member);
+        order.setDelivery(delivery);
+        for (OrderItem orderItem : orderItems) {
+            order.addOrderItem(orderItem);
+        }
+
+        order.setStatus(OrderStatus.ORDER);
+        order.setOrderDate(LocalDateTime.now());
+        return order;
+    }
+
+    // == 비즈니스 로직 == //
+
+    // 주문 취소
+    public void cancel() {
+        if (delivery.getStatus() == DeliveryStatus.COMP) {
+            throw new IllegalStateException("이미 배송완료된 상품은 취소가 불가능합니다.");
+        }
+
+        this.setStatus(OrderStatus.CANCEL);
+
+        for (OrderItem orderItem : orderItems) {
+            orderItem.cancel(); // 각각의 orderItem에 cancel 날려줘야함
+        }
+    }
+
+    // == 조회 로직 == //
+
+    // 전체 주문 가격 조회
+    public int getTotalPrice() {
+        int totalPrice = 0;
+        for (OrderItem orderItem : orderItems) {
+            totalPrice += orderItem.getTotalPrice();
+        }
+        return totalPrice;
+    }
+
 }
