fdf0d553c2584e050daea32d46a9d57009c53e51
Jaehyun Ahn <desktop3711@g.hongik.ac.kr>
2022-11-16 20:59:34 +0900

    Refactor: Add spring container

--- uwoobeat/스프링 핵심 기본편/src/main/java/hello/core/OrderApp.java
+++ uwoobeat/스프링 핵심 기본편/src/main/java/hello/core/OrderApp.java
@@ -8,12 +8,18 @@ import hello.core.order.AppConfig;
 import hello.core.order.Order;
 import hello.core.order.OrderService;
 import hello.core.order.OrderServiceImpl;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 
 public class OrderApp {
     public static void main(String[] args) {
-        AppConfig appConfig = new AppConfig();
-        MemberService memberService = appConfig.memberService();
-        OrderService orderService = appConfig.orderService();
+//        AppConfig appConfig = new AppConfig();
+//        MemberService memberService = appConfig.memberService();
+//        OrderService orderService = appConfig.orderService();
+
+        ApplicationContext applicationContext = new AnnotationConfigApplicationContext(AppConfig.class);
+        MemberService memberService = applicationContext.getBean("memberService", MemberService.class);
+        OrderService orderService = applicationContext.getBean("orderService", OrderService.class);
 
         Long memberId = 1L;
         Member member = new Member(memberId, "memberA", Grade.VIP);
--- uwoobeat/스프링 핵심 기본편/src/main/java/hello/core/order/AppConfig.java
+++ uwoobeat/스프링 핵심 기본편/src/main/java/hello/core/order/AppConfig.java
@@ -6,19 +6,31 @@ import hello.core.member.MemberRepository;
 import hello.core.member.MemberService;
 import hello.core.member.MemberServiceImpl;
 import hello.core.member.MemoryMemberRepository;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
 
+@Configuration
 public class AppConfig {
+
+    @Bean
     public MemberService memberService() {
         return new MemberServiceImpl(memberRepository());
     }
+
+    @Bean
     public OrderService orderService() {
         return new OrderServiceImpl(
                 memberRepository(),
                 discountPolicy());
     }
+
+    @Bean
     public MemberRepository memberRepository() {
         return new MemoryMemberRepository();
     }
+
+    @Bean
     public DiscountPolicy discountPolicy() {
         return new FixDiscountPolicy();
     }
+}
