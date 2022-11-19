cc74ca1139c9fcf3f084e84ef3b235e5ac26f943
ssssujini99 <sssssjin99@naver.com>
2022-11-20 05:07:43 +0900

    :sparkles: Feat: 상품 목록

--- ssssujini99/jpashop/src/main/java/jpabook/jpashop/controller/ItemController.java
+++ ssssujini99/jpashop/src/main/java/jpabook/jpashop/controller/ItemController.java
@@ -2,12 +2,15 @@ package jpabook.jpashop.controller;
 
 
 import jpabook.jpashop.domain.item.Book;
+import jpabook.jpashop.domain.item.Item;
 import jpabook.jpashop.service.ItemService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
+
+import java.util.List;
 
 @Controller
 @RequiredArgsConstructor
@@ -33,5 +36,12 @@ public class ItemController {
 
         itemService.saveItem(book);
         return "redirect:/items";
+    }
+
+    @GetMapping("/items")
+    public String list(Model model) {
+        List<Item> items = itemService.findItems();
+        model.addAttribute("items", items);
+        return "items/itemList";
     }
 }
