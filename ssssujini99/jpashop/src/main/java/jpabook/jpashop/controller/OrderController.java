e36173b5cf0ac17d323ec4c90f1cfdca834005e3
ssssujini99 <sssssjin99@naver.com>
2022-11-20 14:53:53 +0900

    :sparkles: Feat: 주문 취소

--- ssssujini99/jpashop/src/main/java/jpabook/jpashop/controller/OrderController.java
+++ ssssujini99/jpashop/src/main/java/jpabook/jpashop/controller/OrderController.java
@@ -11,10 +11,7 @@ import jpabook.jpashop.service.OrderService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.ModelAttribute;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -56,4 +53,9 @@ public class OrderController {
         return "order/orderList";
     }
 
+    @PostMapping("/orders/{orderId}/cancel")
+    public String cancelOrder(@PathVariable("orderId") Long orderId) {
+        orderService.cancelOrder(orderId);
+        return "redirect:/orders";
+    }
 }
