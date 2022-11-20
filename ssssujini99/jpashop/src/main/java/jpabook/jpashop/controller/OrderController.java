cb299699da1bbece30c8bde8819d4e1c76ce57fd
ssssujini99 <sssssjin99@naver.com>
2022-11-20 14:50:51 +0900

    :sparkles: Feat: 주문 목록 & 목록 검색

--- ssssujini99/jpashop/src/main/java/jpabook/jpashop/controller/OrderController.java
+++ ssssujini99/jpashop/src/main/java/jpabook/jpashop/controller/OrderController.java
@@ -2,7 +2,9 @@ package jpabook.jpashop.controller;
 
 
 import jpabook.jpashop.domain.Member;
+import jpabook.jpashop.domain.Order;
 import jpabook.jpashop.domain.item.Item;
+import jpabook.jpashop.repository.OrderSearch;
 import jpabook.jpashop.service.ItemService;
 import jpabook.jpashop.service.MemberService;
 import jpabook.jpashop.service.OrderService;
@@ -10,6 +12,7 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
@@ -43,5 +46,14 @@ public class OrderController {
         return "redirect:/orders";
     }
 
+
+    @GetMapping("/orders")
+    public String orderList(@ModelAttribute("orderSearch") OrderSearch orderSearch, Model model) {
+
+        List<Order> orders = orderService.findOrders(orderSearch);
+        model.addAttribute("orders", orders);
+
+        return "order/orderList";
+    }
 
 }
--- ssssujini99/jpashop/src/main/java/jpabook/jpashop/service/OrderService.java
+++ ssssujini99/jpashop/src/main/java/jpabook/jpashop/service/OrderService.java
@@ -9,6 +9,7 @@ import jpabook.jpashop.domain.item.Item;
 import jpabook.jpashop.repository.ItemRepository;
 import jpabook.jpashop.repository.MemberRepository;
 import jpabook.jpashop.repository.OrderRepository;
+import jpabook.jpashop.repository.OrderSearch;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
@@ -66,7 +67,7 @@ public class OrderService {
     }
 
     // 3. 검색
-//    public List<Order> findOrders(OrderSearch orderSearch) {
-//        return orderRepository.findAll(orderSearch);
-//    }
+    public List<Order> findOrders(OrderSearch orderSearch) {
+        return orderRepository.findAllByString(orderSearch);
+    }
 }
