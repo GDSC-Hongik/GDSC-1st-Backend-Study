88764e8ef5f79bbdd5da2b73df871ebe2bd403ad
uwoobeat <desktop3711@g.hongik.ac.kr>
2022-11-22 01:46:16 +0900

    Test: Add Spring Bean Test

---
+++ uwoobeat/스프링 핵심 기본편/src/test/java/hello/core/xml/XmlAppContext.java
@@ -0,0 +1,17 @@
+package hello.core.xml;
+
+import hello.core.member.MemberService;
+import org.junit.jupiter.api.Test;
+import org.springframework.context.support.GenericXmlApplicationContext;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+public class XmlAppContext {
+
+    @Test
+    void xmlAppContext() {
+        GenericXmlApplicationContext ac = new GenericXmlApplicationContext("appConfig.xml");
+        MemberService memberService = ac.getBean("memberService", MemberService.class);
+        assertThat(memberService).isInstanceOf(MemberService.class);
+    }
+}
