97881343dbf4fa58383a86e98f72ab9f8b9a2896
younggyo <lee4519698@hanmail.net>
2022-11-24 04:50:04 +0900

    Feat : 주문 조회 V3.1 - 페이징 한계 돌파

--- YoungGyo-00/3. 실전! 스프링 부트와 JPA 활용 1편/src/main/java/jpabook/japshop/api/OrderApiController.java
+++ YoungGyo-00/3. 실전! 스프링 부트와 JPA 활용 1편/src/main/java/jpabook/japshop/api/OrderApiController.java
@@ -5,6 +5,7 @@ import jpabook.japshop.repository.OrderRepository;
 import lombok.Data;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.time.LocalDateTime;
@@ -50,6 +51,17 @@ public class OrderApiController {
         return result;
     }
 
+    @GetMapping("/api/v3.1/orders")
+    public List<OrderDto> orderV3_page(
+            @RequestParam(value = "offset", defaultValue = "0") int offset,
+            @RequestParam(value = "limit", defaultValue = "100") int limit) {
+        List<Order> orders = orderRepository.findAllWithMemberDelivery(offset, limit);
+
+        List<OrderDto> result = orders.stream()
+                .map(o -> new OrderDto(o))
+                .collect(toList());
+        return result;
+    }
 
     @Data
     static class OrderDto {
--- YoungGyo-00/3. 실전! 스프링 부트와 JPA 활용 1편/src/main/java/jpabook/japshop/repository/OrderRepository.java
+++ YoungGyo-00/3. 실전! 스프링 부트와 JPA 활용 1편/src/main/java/jpabook/japshop/repository/OrderRepository.java
@@ -109,4 +109,14 @@ public class OrderRepository {
             " join fetch oi.item i", Order.class)
                 .getResultList();
     }
+
+    public List<Order> findAllWithMemberDelivery(int offset, int limit) {
+        return em.createQuery(
+                "select o from Order o" +
+                        " join fetch o.member m" +
+                        " join fetch o.delivery d", Order.class)
+                .setFirstResult(offset)
+                .setMaxResults(limit)
+                .getResultList();
+    }
 }
